0440455147eba5043a0f7be648428d2578b58b3f
Letao Wang <wlt@Diegos-Mac.local>
2020-10-27 22:20:52 -0400

    release 0.0.3

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # usage
+# git pull
 # update README.md
 # update version number in setuptools.setup
 # python setup.py sdist bdist_wheel
 # twine upload dist/*
 # rm -r build dist *.egg-info
+# git stage, commit and push
 
 import setuptools
 
@@ -12,7 +14,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="hbar",
-    version="0.0.1",
+    version="0.0.3",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator with units",
     long_description=long_description,
