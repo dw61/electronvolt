3fbbd623a5edb7408cd2807e144dfdf92ad71f85
Letao Wang <wlt@Diegos-Mac.local>
2020-10-27 22:25:59 -0400

    remove classifiers, test version 0.0.2 after 0.0.3

--- setup.py
+++ setup.py
@@ -14,17 +14,12 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="hbar",
-    version="0.0.3",
+    version="0.0.2",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator with units",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DiegoWang51/hbar",
     py_modules=["hbar"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
 )
