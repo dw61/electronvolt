fdcf80c525ca574351173b2693f7c8ed09408baa
Letao Wang <wlt@Diegos-Mac.local>
2020-11-05 21:15:08 -0500

    change github username

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="hbar",
-    version="0.2.4",
+    version="0.2.5",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator with units.",
     long_description=long_description,
