161e8a5a9daccbda48e08b918c362d43915d7e23
dw61 <wltok@live.com>
2020-12-09 14:11:20 -0500

    name change

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="electronvolt",
-    version="0.0.3",
+    version="0.0.4",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator.",
     long_description=long_description,
