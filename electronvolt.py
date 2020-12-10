f74032013a7fe456af0ae9d10c0218e68031bc44
dw61 <wltok@live.com>
2020-12-10 13:08:15 -0500

    prepare for final testing

--- electronvolt.py
+++ electronvolt.py
@@ -1,5 +1,5 @@
 # domain and range issues for trigonometry
-# %% double percent sign comment  as hydrogen cell separator
+# %% double percent sign comment as hydrogen cell separator
 
 from math import pi
 from math import e as euler # prevent duplicating elementary charge
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="electronvolt",
-    version="0.1.4",
+    version="0.2.0",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator.",
     long_description=long_description,
