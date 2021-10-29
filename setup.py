89c7d5386b600cbd7f947199178016d1a37073c8
dw61 <wltok@live.com>
2021-10-29 15:48:49 -0400

    bump setup.py to test publish.yml

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="electronvolt",
-    version="1.4.1",
+    version="1.4.2",
     author_email="lw7jz@virginia.edu",
     description="A physical quantity calculator.",
     long_description=long_description,
