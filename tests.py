b4a942a5c5b57edd9e83880910ff98a664060122
dw61 <wltok@live.com>
2021-10-31 12:45:31 -0400

    spacey tests

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 from electronvolt import *
 
-assert c == 299792458*m/s
-assert euler**(1*s/s) == exp(1*kg/kg)
+assert c == 299792458 * m / s
+assert euler ** (1 * s / s) == exp(1 * kg / kg)
 assert cos(-pi) == -1
-assert 12 != 12*kg
-assert 2 == 1*kg/kg + 1
-assert Quantity(6, kg=5, mol=-2) == 6*kg**5/mol**2
+assert 12 != 12 * kg
+assert 2 == 1 * kg / kg + 1
+assert Quantity(6, kg=5, mol=-2) == 6 * kg**5 / mol**2
 
-assert 1*m != 1*s
-assert 2*kg + kg*2 <= 5*kg
-assert N/C == V/m
-assert kg*c**2 in J
+assert 1 * m != 1 * s
+assert 2 * kg + kg * 2 <= 5 * kg
+assert N / C == V / m
+assert eV in J
 assert pc > ly
