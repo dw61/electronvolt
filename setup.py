4fafbab5994b08338f4c9b0e64f6b57a9fbef8b5
dw61 <wltok@live.com>
2021-12-03 22:06:01 -0500

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="electronvolt",
-    version="1.5.10",
+    version="1.5.11",
     author_email="lw7jz@virginia.edu",
     description="A physics quantity calculator with units.",
     long_description=long_description,
